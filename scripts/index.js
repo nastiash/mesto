56670ee5b61e56668ec4bfe541d55a7b52ae5d9f
Nastia Shuyskaya <anst.shy@yandex.ru>
2020-11-24 17:23:53 +0300

    исправления после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,6 @@ let editProfileButton = document.querySelector('.profile__edit-button');
 let profilePopup = document.querySelector('.pop-up');
 let closeProfilePopup = document.querySelector('.pop-up__close-button');
 
-editProfileButton.addEventListener('click', openPopup);
-closeProfilePopup.addEventListener('click', closePopup);
-
 let profileForm = document.querySelector('.pop-up__form');
 let inputProfileName = document.querySelector('.pop-up__form-item_el_name');
 let inputProfileCaption = document.querySelector('.pop-up__form-item_el_caption');
@@ -12,7 +9,7 @@ let submitProfileFormButton = document.querySelector('.pop-up__submit-button');
 let profileName = document.querySelector('.profile__name');
 let profileCaption = document.querySelector('.profile__caption');
 
-profilePopup.addEventListener('submit', submitProfileForm);
+
 
 // открыть и закрыть поп-ап
 function openPopup() {
@@ -31,5 +28,10 @@ function submitProfileForm (event) {
   event.preventDefault();
   profileName.textContent = inputProfileName.value;
   profileCaption.textContent = inputProfileCaption.value;
-  profilePopup.classList.remove('pop-up_opened');
+  closePopup();
 }
+
+editProfileButton.addEventListener('click', openPopup);
+closeProfilePopup.addEventListener('click', closePopup);
+
+profilePopup.addEventListener('submit', submitProfileForm);
